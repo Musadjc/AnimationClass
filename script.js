11eeb4a3e971d308fb9b33754db12b5046eebe25
Musa Djiguel <musadjiguel@gmail.com>
2024-01-31 23:36:49 +0000

    I can move the both box in diagonal

--- script.js
+++ script.js
@@ -17,15 +17,35 @@ let interval = setInterval(() =>{
 
 //El que hizo el profe
 //const box = document.getElementById('box')
-let boxTop = 0
+let boxTop1 = 0
 let botTopLeft = 0
+let boxTop2 = 0
+let boxTopRigth = 400
+let canvasHeight = 600
+let canvaswidht = 600
 
+
+//Box 1
 setInterval(() => {
-    boxTop += 5
-    box.style.top = `${boxTop}px`
+    boxTop1 += 5
+    box.style.top = `${boxTop1}px`
 }, 200)
 
 setInterval(() => {
-    boxTop += 5
-    box.style.left= `${boxTop}px`
+    boxTop1 += 5
+    box.style.left = `${boxTop1}px`
 }, 200)
+
+//Box 2
+setInterval(() => {
+    boxTop2 += 5
+    box2.style.top = `${boxTop2}px`
+}, 200)
+
+setInterval(() => {
+    boxTop2 += 5
+    box2.style.right = `${boxTop2}px`
+}, 200)
+
+
+
